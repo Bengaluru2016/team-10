8ab556f648c07b83cbd373b8ad24e8f01d1c05b7
rashmithagaddam <Rashmithagaddam@gmail.com>
2016-07-10 02:27:31 +0530

    domains

---
+++ second.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+  <div>
+  <b> CHOOSE 1!!!!!!</b>
+  </div>
+  <div class="container-fluid bg-3 text-center"> 
+  <h3>Where To Find Me?</h3>
+  <div class="row">
+  <div class="row">
+    <div class="col-sm-4">
+      <p>Lorem ipsum..</p>
+    <a href="http://fACEBOOK.COM"><img src="HEALTHCARE.jpeg" alt="Image"> </a>
+    </div>
+    <div class="col-sm-4">
+      <p>Lorem ipsum..</p>
+    <a href="http://fACEBOOK.COM"><img src="HEALTHCARE(1).jpeg" alt="Image"> </a>
+    </div>
+    <div class="col-sm-4"> 
+      <p>Lorem ipsum..</p>
+      <a href="http://fACEBOOK.COM"><img src="HEALTYHCARE2.jpeg" alt="Image"> </a>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-6">
+      <p>Lorem ipsum..</p>
+       <a href="http://fACEBOOK.COM"><img src="HEALTHCARE3.jpeg" alt="Image"> </a>
+    </div>
+    <div class="col-sm-6">
+      <p>Lorem ipsum..</p>
+      <a href="http://fACEBOOK.COM"><img src="HEALTHCARE4.jpeg" alt="Image"> </a>
+    </div>
+  </div>
+</div>
+</div>
+ </body>
+ </html>
