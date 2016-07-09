1f31e7965c2c6c01af47db017ba2d4bfbfcef103
vanithasajjan <vanithasajjanshetty@gmail.com>
2016-07-09 23:04:39 +0530

    quiz
    quiz

---
+++ Quiz/grade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	
+	<title>PHP Quiz</title>
+	
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+
+<body>
+
+	<div id="page-wrap">
+
+		<h1>Final Quiz for Lip building</h1>
+		
+        <?php
+            $host = 'localhost';
+			$user = 'root';
+			$pwd = '';
+			$db = 'learningms';
+
+	$conn = mysql_connect($host,$user,$pwd);
+	if(!$conn)
+		die("Failed to connect".mysql_error());
+	echo "connected";
+
+	if(!mysql_select_db($db,$conn))
+		die(mysql_error());
+
+
+            $answer1 = $_POST['question-1-answers'];
+            $answer2 = $_POST['question-2-answers'];
+            $answer3 = $_POST['question-3-answers'];
+            $answer4 = $_POST['question-4-answers'];
+            $answer5 = $_POST['question-5-answers'];
+        
+            $totalCorrect = 0;
+            
+            if ($answer1 == "B") { $totalCorrect++; }
+            if ($answer2 == "A") { $totalCorrect++; }
+            if ($answer3 == "C") { $totalCorrect++; }
+            if ($answer4 == "D") { $totalCorrect++; }
+            if ($answer5) { $totalCorrect++; }
+            
+            echo "<div id='results'>$totalCorrect / 5 correct</div>";
+
+            $insert = "INSERT INTO quiz VALUES(1,1,1,1,1,'$totalCorrect','')";
+
+	$run_query = mysql_query($insert,$conn);
+
+		if(!$run_query)
+			echo "Failed to insert values\n";
+            
+        ?>
+	
+	</div>
+	
+	<script type="text/javascript">
+	var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+	document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+	</script>
+	<script type="text/javascript">
+	var pageTracker = _gat._getTracker("UA-68528-29");
+	pageTracker._initData();
+	pageTracker._trackPageview();
+	</script>
+
+</body>
+
+</html>
